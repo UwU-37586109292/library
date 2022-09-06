d8fafca48333a5f82d3738e3d6275270cef72793
Zuzka <kikucisko@gmail.com>
2022-09-06 14:38:53 +0200

    Add Book constructor and info method, add addBookToLibrary method

---
+++ library.js
@@ -0,0 +1,19 @@
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+Book.prototype.info = function () {
+    const readStr = this.isRead ? 'already read' : 'not yet read';
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStr}`;
+}
+
+let myLibrary = [];
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
